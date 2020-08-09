c2c3aa614cbfd6a969984795037d55cf4770a795
Tuukka Veteli <tuukkaveteli@gmail.com>
2020-08-09 17:17:53 +0300

    added login and ability to add blogs through front end

--- part5/blog_front/src/App.js
+++ part5/blog_front/src/App.js
@@ -1,11 +1,17 @@
 import React, { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
+  const [errorMessage, setErrorMessage] = useState('')
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -13,12 +19,120 @@ const App = () => {
     )
   }, [])
 
-  return (
+  const addBlog = (event) => {
+    event.preventDefault()
+    const blog = {
+      title: title,
+      author: author,
+      url: url,
+      likes: 0
+    }
+    blogService.create(blog)
+      .then(res => { setBlogs(blogs.concat(res)) })
+  }
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    try {
+      const user = await loginService.login({ username, password })
+      blogService.setToken(user.token)
+      console.log(user)
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      setErrorMessage('wrong credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+  }
+
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div>
+        username:
+            <input
+          type="text"
+          value={username}
+          name="username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password:
+            <input
+          type="text"
+          value={password}
+          name="password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">login</button>
+    </form>
+  )
+
+
+  const blogForm = () => (
+    <form onSubmit={addBlog}>
+      <h3>Add new blog</h3>
+
+      <div>
+        Title:
+        <input
+          type='text'
+          value={title}
+          name='title'
+          onChange={({ target }) => setTitle(target.value)}
+        />
+      </div>
+
+      <div>
+        Author:
+        <input
+          type='text'
+          value={author}
+          name='author'
+          onChange={({ target }) => setAuthor(target.value)}
+        />
+      </div>
+
+      <div>
+        url:
+        <input
+          type='text'
+          value={url}
+          name='url'
+          onChange={({ target }) => setUrl(target.value)}
+        />
+      </div>
+
+      <button type="submit">save</button>
+    </form>
+  )
+
+  const blogList = () => (
     <div>
-      <h2>blogs</h2>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
+    </div>
+  )
+
+
+  return (
+    <div>
+      <h1>Blogs</h1>
+      {/* <Notification message={errorMessage} /> */}
+      {user === null ?
+        loginForm() :
+        <div>
+          <p>{user.name} logged in</p>
+          {blogForm()}
+          <h2>Blogs</h2>
+          {blogList()}
+        </div>
+      }
     </div>
   )
 }
