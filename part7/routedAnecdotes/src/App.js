7910f483b9171b14ec7ec4a2834ba09152312c01
stuge <tuukkaveteli@gmail.com>
2021-02-11 18:30:53 +0200

    7.4, 7.5, 7.7 done from 7 B 7.7 and 7.8 to do

--- part7/routedAnecdotes/src/App.js
+++ part7/routedAnecdotes/src/App.js
@@ -3,10 +3,10 @@ import {
   Switch,
   Route,
   Link,
-  Redirect,
   useRouteMatch,
   useHistory,
 } from "react-router-dom"
+import {useField} from './hooks/index'
 
 const Menu = () => {
   const padding = {
@@ -81,41 +81,51 @@ const Footer = () => (
 )
 
 const CreateNew = (props) => {
-  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
+  const {reset:contentReset, ...content} = useField('text')
+  const {reset:authorReset, ...author} = useField('text')
+  const {reset:infoReset, ...info} = useField('text')
+
   const history = useHistory()
 
   const handleSubmit = (e) => {
     e.preventDefault()
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0
     })
     history.push('/')
-    props.setNotification(`a New anecdote: ${content} created!`)
+    props.setNotification(`a New anecdote: "${content.value}" created!`)
     setTimeout(() => {props.setNotification('')}, 10000)
+  }
+
+  const handleReset = (e) => {
+    e.preventDefault()
+    contentReset()
+    authorReset()
+    infoReset()
   }
 
   return (
     <div>
       <h2>create a new anecdote</h2>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} onReset={handleReset}>
         <div>
           content
-          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+
+          <input {...content} />
         </div>
         <div>
           author
-          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+          <input {...author} />
         </div>
         <div>
           url for more info
-          <input name='info' value={info} onChange={(e)=> setInfo(e.target.value)} />
+          <input {...info} />
         </div>
-        <button>create</button>
+        <button type='submit'>create</button>
+        <button type='reset'>reset</button>
       </form>
     </div>
   )
