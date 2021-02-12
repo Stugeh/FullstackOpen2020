8a8aa7f3b21f8a24b137fddd966698eda07466ce
stuge <tuukkaveteli@gmail.com>
2021-02-12 16:46:58 +0200

    added bloglist client and server code to part 7

---
+++ part7/blogList/server/models/blog.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+const config = require('../utils/config')
+mongoose.set('useFindAndModify', false)
+mongoose.set('useCreateIndex', true)
+
+const blogSchema = mongoose.Schema({
+    title: { type: String, required: true },
+    author: String,
+    url: { type: String, required: true },
+    likes: { type: Number, default: 0 },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }
+})
+
+blogSchema.set('toJSON', {
+    transform: (document, returnedObj) => {
+        returnedObj.id = returnedObj._id.toString()
+        delete returnedObj._id
+        delete returnedObj.__v
+    },
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+module.exports = Blog
