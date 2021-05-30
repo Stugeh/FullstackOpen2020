52de832f0157014cc68d5b009177d38d8ad011fe
stuge <tuukkaveteli@gmail.com>
2021-05-30 12:40:27 +0300

    diagnoses endpoint

--- part9/Patientor/src/index.ts
+++ part9/Patientor/src/index.ts
@@ -2,9 +2,13 @@ import express from 'express';
 const app = express();
 import cors from 'cors';
 
+import diagnoseRouter from './routes/diagnoses';
+
 app.use(express.json());
 // eslint-disable-next-line @typescript-eslint/no-unsafe-call
 app.use(cors());
+
+app.use('/api/diagnoses', diagnoseRouter);
 
 const PORT = 3001;
 
