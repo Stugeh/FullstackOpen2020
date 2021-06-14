653a31b9e571b6c2921e50449001629cc3d78eb3
Stuge <tuukkaveteli@gmail.com>
2021-06-14 17:06:19 +0300

    validation for form fields

--- part9/patientor-front/src/AddEntryModal/AddEntryForm.tsx
+++ part9/patientor-front/src/AddEntryModal/AddEntryForm.tsx
@@ -2,12 +2,13 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import React from 'react';
 
-import {Button} from 'semantic-ui-react';
+import {Button, Grid} from 'semantic-ui-react';
 
 import {Field, Formik, Form} from 'formik';
 import {useStateValue} from '../state';
 import { HealthCheckRating, NewHealthCheckEntry, Patient } from '../types';
 import {DiagnosisSelection, NumberField, TextField} from '../AddPatientModal/FormField';
+import { isDate } from 'util';
 
 interface Props {
     onSubmit: (values: NewHealthCheckEntry) => void;
@@ -27,13 +28,45 @@ const AddEntryForm = ({ onSubmit, onCancel }: Props) => {
                 type: 'HealthCheck'
             }}
             onSubmit={onSubmit}
+            validate={values => {
+                const requiredError = "Field is required";
+                const dateError = "Not a valid date";
+                const errors: { [field: string]: string } = {};
+                if (!values.description) {
+                    errors.description = requiredError;
+                }
+                if (!values.date) {
+                    errors.date = requiredError;
+                }
+                if (!values.specialist) {
+                    errors.specialist = requiredError;
+                }
+                if (values.date.length !== 10 || !Date.parse(values.date))
+                    errors.date = dateError;
+                return errors;
+              }}
         >
             {({ isValid, dirty, setFieldValue, setFieldTouched }) => {
                 return (
                     <Form className="form ui">
-                        <Field label="description" name="description" component={TextField}/>
-                        <Field label="date" name="date" component={TextField} />
-                        <Field label="specialist" name="specialist" component={TextField} />
+                        <Field
+                            label="description"
+                            name="description"
+                            component={TextField}
+                            placeholder='description'
+                        />
+                        <Field
+                            label="date"
+                            name="date"
+                            component={TextField}
+                            placeholder="YYYY-MM-DD"
+                        />
+                        <Field
+                            label="specialist"
+                            name="specialist"
+                            component={TextField}
+                            placeholder="specialist"
+                        />
                         <DiagnosisSelection
                             setFieldValue={setFieldValue}
                             setFieldTouched={setFieldTouched}
@@ -46,14 +79,23 @@ const AddEntryForm = ({ onSubmit, onCancel }: Props) => {
                             min={0}
                             max={3}
                         />
-                        <Button
-                          type="submit"
-                          floated="right"
-                          color="green"
-                          disabled={!dirty || !isValid}
-                        >
-                          Add
-                        </Button>
+                        <Grid>
+                            <Grid.Column floated="left" width={5}>
+                              <Button type="button" onClick={onCancel} color="red">
+                                Cancel
+                              </Button>
+                            </Grid.Column>
+                            <Grid.Column floated="right" width={5}>
+                              <Button
+                                type="submit"
+                                floated="right"
+                                color="green"
+                                disabled={!dirty || !isValid}
+                              >
+                                Add
+                              </Button>
+                            </Grid.Column>
+                        </Grid>
                     </Form>
                 );
             }}
