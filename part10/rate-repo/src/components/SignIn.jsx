16fa0c2335ad984b69d7c0065cceed91a4655a43
stuge <tuukkaveteli@gmail.com>
2021-07-04 10:35:58 +0300

    Added skeleton for login form

--- part10/rate-repo/src/components/SignIn.jsx
+++ part10/rate-repo/src/components/SignIn.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import {View} from 'react-native';
+import Formik from 'formik';
 
 import Text from './Text';
 
+const LoginForm = () => {
+  return (
+    <View>
+
+    </View>
+  );
+};
+
 const SignIn = () => {
-  return <Text>The sign in view</Text>;
+  return (
+    <Formik initialValues={initialValues} onSubmit={onSubmit}>
+      {({ handleSubmit }) => <LoginForm onSubmit={handleSubmit} />}
+    </Formik>
+  );
 };
 
 export default SignIn;
