a841b0c924de3d01b35a9d429a08ee6134c5d90f
stuge <tuukkaveteli@gmail.com>
2021-07-04 12:50:43 +0300

    every text field now uses the unified styles

--- part10/rate-repo/src/components/TextInput.jsx
+++ part10/rate-repo/src/components/TextInput.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { TextInput as NativeTextInput, StyleSheet } from 'react-native';
 
+import theme from '../theme';
+
 const styles = StyleSheet.create({});
 
 const TextInput = ({ style, error, ...props }) => {
   const textInputStyle = [style];
 
-  return <NativeTextInput style={textInputStyle} {...props} />;
+  return <NativeTextInput style={theme.textField} {...props} />;
 };
 
 export default TextInput;
