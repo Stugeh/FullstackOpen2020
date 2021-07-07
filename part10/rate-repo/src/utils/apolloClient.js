1e0ae285fb59d3473f10a2c7582f9b5089c1960b
stuge <tuukkaveteli@gmail.com>
2021-07-07 13:51:51 +0300

    fix wrong link

--- part10/rate-repo/src/utils/apolloClient.js
+++ part10/rate-repo/src/utils/apolloClient.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 
 const httpLink = createHttpLink({
-  uri: 'http://192.168.1.5:5000/api/repositories',
+  uri: 'http://192.168.1.5:5000/graphql',
 });
 
 const createApolloClient = () => {
