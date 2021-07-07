e8b9781ddc0f91a551d83f7c94cbe161ad07ace2
stuge <tuukkaveteli@gmail.com>
2021-07-07 18:23:12 +0300

    populated the class methods for auth tokens

--- part10/rate-repo/src/utils/authStorage.js
+++ part10/rate-repo/src/utils/authStorage.js
@@ -5,16 +5,22 @@ class AuthStorage {
     this.namespace = namespace;
   }
 
-  getAccessToken() {
-    // Get the access token for the storage
+  async getAccessToken () {
+    const token = await AsyncStorage.getItem(
+      `${this.namespace}:token`
+    );
+    return token ? token : undefined;
   }
 
-  setAccessToken(accessToken) {
-    // Add the access token to the storage
+  async setAccessToken(accessToken) {
+    await AsyncStorage.setItem(
+      `${this.namespace}:token`,
+      accessToken
+    );
   }
 
-  removeAccessToken() {
-    // Remove the access token from the storage
+  async removeAccessToken() {
+    await AsyncStorage.removeItem(`${this.namespace}:token`);
   }
 }
 
