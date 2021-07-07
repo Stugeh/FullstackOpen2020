3e975f9612133d9a708364886f680d67db553140
stuge <tuukkaveteli@gmail.com>
2021-07-07 13:50:05 +0300

    fixed ip in apollo client

--- part10/rate-repo/src/utils/apolloClient.js
+++ part10/rate-repo/src/utils/apolloClient.js
@@ -1,8 +1,7 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 
 const httpLink = createHttpLink({
-  // Replace the IP address part with your own IP address!
-  uri: 'http://192.168.100.16:5000/graphql',
+  uri: 'http://192.168.1.5:5000/api/repositories',
 });
 
 const createApolloClient = () => {
