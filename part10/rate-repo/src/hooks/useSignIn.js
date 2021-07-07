2c3d45e79fbfced1cbf17b1430060a733c3de8f5
stuge <tuukkaveteli@gmail.com>
2021-07-07 17:24:31 +0300

    sign in hook done

--- part10/rate-repo/src/hooks/useSignIn.js
+++ part10/rate-repo/src/hooks/useSignIn.js
@@ -1,11 +1,14 @@
 import { useMutation } from "@apollo/client";
+import SIGN_IN from '../graphql/mutations';
 
 const useSignIn = () => {
-  const [mutate, result] = useMutation(/* mutation arguments */);
-  
+  const [login, result] = useMutation(SIGN_IN);
+
   const signIn = async ({ username, password }) => {
-    // call the mutate function here with the right arguments
+    login({variables: {username, password}});
+    return result.data?.authorize?.accessToken;
   };
-  
-  return [signIn, result];
+  return [signIn, result.data];
 };
+
+export default useSignIn;
