b947e28cf4d0b8f8bfb6c31c52b68e373cb540c2
stuge <tuukkaveteli@gmail.com>
2021-07-07 15:14:27 +0300

    refactored to use graphql

--- part10/rate-repo/src/hooks/useRepositories.jsx
+++ part10/rate-repo/src/hooks/useRepositories.jsx
@@ -1,25 +1,22 @@
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
+import {useLazyQuery} from '@apollo/client';
+import {GET_REPOSITORIES} from '../graphql/queries';
 
 const useRepositories = () => {
-  const [repositories, setRepositories] = useState();
-  const [loading, setLoading] = useState(false);
-
-  const fetchRepositories = async () => {
-    setLoading(true);
-
-    // Replace the IP address part with your own IP address!
-    const response = await fetch('http://192.168.1.5:5000/api/repositories');
-    const json = await response.json();
-
-    setLoading(false);
-    setRepositories(json);
-  };
+  const [fetchRepositories, { loading, data, refetch }] = useLazyQuery(
+    GET_REPOSITORIES,
+    {fetchPolicy: 'cache-and-network'}
+  );
 
   useEffect(() => {
     fetchRepositories();
   }, []);
 
-  return { repositories, loading, refetch: fetchRepositories };
+  return { 
+    repositories: data ? data.repositories : undefined,
+    loading,
+    refetch
+  };
 };
 
 export default useRepositories;
