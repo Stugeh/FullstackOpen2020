f550b3685f728da8e1cd334d1ff49aa41352a7ad
stuge <tuukkaveteli@gmail.com>
2021-07-07 14:03:44 +0300

    added apollo provider to the app

--- part10/rate-repo/App.js
+++ part10/rate-repo/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
 import Main from './src/components/Main';
+
 import { NativeRouter } from 'react-router-native';
+import { ApolloProvider } from '@apollo/client';
+
+import { StyleSheet, View } from 'react-native';
+
+import createApolloClient from './src/utils/apolloClient';
+
+const apolloClient = createApolloClient();
 
 export default function App() {
   return (
     <NativeRouter>
-      <View style={styles.container}>
-        <Main/>
-      </View>
+      <ApolloProvider client={apolloClient}>
+        <View style={styles.container}>
+          <Main/>
+        </View>
+      </ApolloProvider>
     </NativeRouter>
   );
 }
