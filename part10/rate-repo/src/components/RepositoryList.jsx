24b75ba269be0f3aa6efac4a74bdc4266924a6d8
stuge <tuukkaveteli@gmail.com>
2021-07-07 13:45:16 +0300

    refactored to use the new useRepositories hook

--- part10/rate-repo/src/components/RepositoryList.jsx
+++ part10/rate-repo/src/components/RepositoryList.jsx
@@ -1,7 +1,8 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import { FlatList, View, StyleSheet } from 'react-native';
 import theme from '../theme';
 import RepositoryItem from './RepositoryItem';
+import useRepositories from '../hooks/useRepositories';
 
 
 const styles = StyleSheet.create({
@@ -17,22 +18,12 @@ const styles = StyleSheet.create({
 const ItemSeparator = () => <View style={styles.separator} />;
 
 const RepositoryList = () => {
-  const [repositories, setRepositories] = useState([]);
-
-  const fetchRepositories = async () => {
-    const res = await fetch('http://192.168.1.5:5000/api/repositories');
-    const json = await res.json();
-    setRepositories(json);
-  };
-
-  useEffect(()=>{
-    fetchRepositories();
-  },[]);
-
-  const repositoryNodes = repositories.edges
+  const { repositories } = useRepositories();
+  
+  const repositoryNodes = repositories
     ? repositories.edges.map(edge => edge.node)
     : [];
-
+    
   return (
     <FlatList
       data={repositoryNodes}
