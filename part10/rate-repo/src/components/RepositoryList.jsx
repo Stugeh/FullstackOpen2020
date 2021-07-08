d5c5b3701ba82817b8f1893102b8e0c105fcaf3d
stuge <tuukkaveteli@gmail.com>
2021-07-08 13:51:38 +0300

    seperated pure code to its own component

--- part10/rate-repo/src/components/RepositoryList.jsx
+++ part10/rate-repo/src/components/RepositoryList.jsx
@@ -14,9 +14,7 @@ const styles = StyleSheet.create({
 
 const ItemSeparator = () => <View style={styles.separator} />;
 
-const RepositoryList = () => {
-  const { repositories } = useRepositories();
-
+export const RepositoryListContainer = ({repositories}) => {
   const repositoryNodes = repositories
     ? repositories.edges.map(edge => edge.node)
     : [];
@@ -30,6 +28,11 @@ const RepositoryList = () => {
       contentContainerStyle={{ paddingBottom: 75 }}
     />
   );
+};
+
+const RepositoryList = () => {
+  const { repositories } = useRepositories();
+  return <RepositoryListContainer repositories={repositories}/>;
 };
 
 export default RepositoryList;
