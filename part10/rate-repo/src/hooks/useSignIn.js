c9abe30658d8ad869dfca541a4c13afa39f52b6e
stuge <tuukkaveteli@gmail.com>
2021-07-08 11:44:04 +0300

    made the sign in hook less janky.

--- part10/rate-repo/src/hooks/useSignIn.js
+++ part10/rate-repo/src/hooks/useSignIn.js
@@ -1,24 +1,25 @@
-import { useMutation } from "@apollo/client";
+import { useMutation, useApolloClient } from "@apollo/client";
 import SIGN_IN from '../graphql/mutations';
-import apolloClient from '../utils/apolloClient';
 
 import useAuthStorage from '../hooks/useAuthStorage';
 
 const useSignIn = () => {
   const authStorage = useAuthStorage();
-  const [login, result] = useMutation(SIGN_IN);
-
+  const apolloClient = useApolloClient();
+  const [login, {data}] = useMutation(SIGN_IN);
+  const accessToken = data?.authorize 
+    ? data.authorize.accessToken 
+    : undefined;
+  
   const signIn = async ({ username, password }) => {
     await login({variables: {username, password}});
-    const {accessToken} = result.data.authorize;
     if(accessToken){
       await authStorage.setAccessToken(accessToken);
       apolloClient.resetStore();
-      return authStorage.getAccessToken(); 
     }
-    return undefined;
+    return accessToken;
   };
-  return [signIn, result.data];
+  return [signIn, accessToken];
 };
 
 export default useSignIn;
