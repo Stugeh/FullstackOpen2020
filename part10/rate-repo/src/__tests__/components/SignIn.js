a939b44c621ac2b1a2cba2a3d5befe94ad6abaf9
stuge <tuukkaveteli@gmail.com>
2021-07-10 14:22:30 +0300

    created a test for SignIn form

--- part10/rate-repo/src/__tests__/components/SignIn.js
+++ part10/rate-repo/src/__tests__/components/SignIn.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import { render, fireEvent, waitFor } from '@testing-library/react-native';
-import SignIn from '../../components/SignIn';
+import {SignInContainer} from '../../components/SignIn';
 
 describe('SignIn', () => {
   describe('SignInContainer', () => {
     it('calls onSubmit function with correct arguments when a valid form is submitted', async () => {
-      // render the SignInContainer component, fill the text inputs and press the submit button
-
+      const mockSubmit = jest.fn();
+      const {getByTestId} = render(<SignInContainer onSubmit={mockSubmit}/>);
+      
+      fireEvent.changeText(getByTestId('usernameInput'), 'kalle');
+      fireEvent.changeText(getByTestId('passwordInput'), 'password');
+      fireEvent.press(getByTestId('loginSubmitButton'));
       await waitFor(() => {
-        // expect the onSubmit function to have been called once and with a correct first argument
+        expect(mockSubmit.mock.calls).toHaveLength(1);
+        expect(mockSubmit.mock.calls[0][0]).toEqual({
+          username: 'kalle',
+          password: 'password',
+        });
       });
     });
   });
