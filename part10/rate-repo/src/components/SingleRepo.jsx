ea90245ea73bc3d1251a9f1267ce6d7a6e831a68
stuge <tuukkaveteli@gmail.com>
2021-07-11 13:43:00 +0300

    fixed bug with separators not rendering

--- part10/rate-repo/src/components/SingleRepo.jsx
+++ part10/rate-repo/src/components/SingleRepo.jsx
@@ -3,15 +3,18 @@ import {View, FlatList} from 'react-native';
 import {useParams} from 'react-router-native';
 import {useQuery} from '@apollo/client';
 
+import theme from '../theme';
+
 import { GET_REPO_BY_ID } from '../graphql/queries';
 import {Heading} from '../components/Text';
 import { RepositoryItemContainer } from './RepositoryList/RepositoryItem';
 
 const ReviewItem = ({review}) => {
+  console.log(`object`, review);
   const {text, rating, createdAt, user} = review;
   return (
     <View>
-      
+      <Heading>{user.username}</Heading>
     </View>
   );
 };
@@ -25,21 +28,23 @@ const SingleRepo = () => {
       fetchPolicy:'cache-and-network'
     }
   );
-  const reviews = data?.reviews?.edges.map(edge => edge.node);
-  
+  console.log(`data`, Object.keys(data));
+  const reviews = data?.repository?.reviews?.edges.map(edge => edge.node);
+  console.log(`reviews`, reviews);
   if (loading) return <Heading>loading</Heading>;
   if(error) return <Heading>Error occurred</Heading>;
-  
   return (
     <FlatList
       data={reviews}
       renderItem={({ item }) => <ReviewItem review={item} />}
       keyExtractor={({ id }) => id}
       ListHeaderComponent={() =>       
-        <RepositoryItemContainer 
-          item={data.repository}
-          singleRepoView
-        />
+        <RepositoryItemContainer
+          item={data.repository} singleRepoView />
+      }
+      ItemSeparatorComponent={() =>
+        <View style={{
+          backgroundColor: theme.colors.separator, height: 5}}/>
       }
     />
 
