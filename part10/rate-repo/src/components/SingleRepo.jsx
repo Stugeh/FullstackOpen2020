0905410593751a6aef1cdbc0c26ed2791bdff0d2
stuge <tuukkaveteli@gmail.com>
2021-07-11 14:24:53 +0300

    review items now rendered properly

--- part10/rate-repo/src/components/SingleRepo.jsx
+++ part10/rate-repo/src/components/SingleRepo.jsx
@@ -1,23 +1,58 @@
 import React from 'react';
-import {View, FlatList} from 'react-native';
+import {View, FlatList, StyleSheet} from 'react-native';
 import {useParams} from 'react-router-native';
 import {useQuery} from '@apollo/client';
 
 import theme from '../theme';
 
 import { GET_REPO_BY_ID } from '../graphql/queries';
-import {Heading} from '../components/Text';
+import Text, {Heading} from '../components/Text';
 import { RepositoryItemContainer } from './RepositoryList/RepositoryItem';
 
+const styles = StyleSheet.create({
+  reviewContainer:{
+    flex:1,
+    backgroundColor: 'white',
+    padding: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+  },
+  rating:{
+    height: 50,
+    width: 50,
+    lineHeight: 50,
+    textAlign: 'center',
+    marginRight:5,
+    borderRadius: 30,
+    borderWidth:3,
+    borderColor: theme.colors.primary,
+    color: theme.colors.primary,
+  },
+  body:{
+    flexShrink:1,
+  },
+  separator: {
+    height: 5,
+    backgroundColor: theme.colors.separator
+  }
+});
+
 const ReviewItem = ({review}) => {
-  console.log(`object`, review);
   const {text, rating, createdAt, user} = review;
   return (
-    <View>
-      <Heading>{user.username}</Heading>
+    <View style={styles.reviewContainer}>
+      <Heading style={styles.rating}>
+        {rating}
+      </Heading>
+      <View style={styles.body}>
+        <Heading>{user.username}</Heading>
+        <Text>{createdAt}</Text>
+        <Text>{text}</Text>
+      </View>
     </View>
   );
 };
+const Separator = () => (<View style={styles.separator}/>);
 
 const SingleRepo = () => {
   const {id} = useParams();
@@ -28,9 +63,10 @@ const SingleRepo = () => {
       fetchPolicy:'cache-and-network'
     }
   );
-  console.log(`data`, Object.keys(data));
+
+
   const reviews = data?.repository?.reviews?.edges.map(edge => edge.node);
-  console.log(`reviews`, reviews);
+
   if (loading) return <Heading>loading</Heading>;
   if(error) return <Heading>Error occurred</Heading>;
   return (
@@ -38,13 +74,14 @@ const SingleRepo = () => {
       data={reviews}
       renderItem={({ item }) => <ReviewItem review={item} />}
       keyExtractor={({ id }) => id}
-      ListHeaderComponent={() =>       
-        <RepositoryItemContainer
-          item={data.repository} singleRepoView />
-      }
-      ItemSeparatorComponent={() =>
-        <View style={{
-          backgroundColor: theme.colors.separator, height: 5}}/>
+      ItemSeparatorComponent={Separator}
+      ListHeaderComponent={() => (
+        <View> 
+          <RepositoryItemContainer
+            item={data.repository} singleRepoView />
+          <Separator/>
+        </View>
+      )
       }
     />
 
