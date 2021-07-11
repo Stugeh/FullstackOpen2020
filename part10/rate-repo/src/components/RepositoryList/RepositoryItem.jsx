efa2c1ec8b5b387b3d1ca5fed737b396d717d010
stuge <tuukkaveteli@gmail.com>
2021-07-11 11:31:46 +0300

    removed useState import

--- part10/rate-repo/src/components/RepositoryList/RepositoryItem.jsx
+++ part10/rate-repo/src/components/RepositoryList/RepositoryItem.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 import {View,Image , StyleSheet, Pressable} from 'react-native';
 import {useHistory} from 'react-router-native';
 
