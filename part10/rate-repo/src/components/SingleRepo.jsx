30f6a8531167b3061e54844b214930428734416e
stuge <tuukkaveteli@gmail.com>
2021-07-11 12:17:24 +0300

    fetches the repo info from server

--- part10/rate-repo/src/components/SingleRepo.jsx
+++ part10/rate-repo/src/components/SingleRepo.jsx
@@ -1,19 +1,26 @@
 import React from 'react';
-import {View} from 'react-native';
 import {useParams} from 'react-router-native';
 import {useQuery} from '@apollo/client';
 
-import { GET_REPO_URL } from '../graphql/queries';
+import { GET_REPO_BY_ID } from '../graphql/queries';
+import {Heading} from '../components/Text';
+import { RepositoryItemContainer } from './RepositoryList/RepositoryItem';
 
 const SingleRepo = () => {
   const {id} = useParams();
-  const [getRepoUrl, {data, loading, error}] = useQuery(GET_REPO_URL);
-  console.log(`id`, id);
+  const {data, loading, error} = useQuery(
+    GET_REPO_BY_ID,
+    {   
+      variables: {id},
+      fetchPolicy:'cache-and-network'
+    }
+  );
+
+  if(error) return <Heading>Error occurred</Heading>;
+  if (loading) return <Heading>loading</Heading>;
 
   return (
-    <View>
-            
-    </View>
+    <RepositoryItemContainer item={data.repository}/>
   );
 };
 
