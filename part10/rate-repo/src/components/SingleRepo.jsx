209834323c107e0f7698e3117f0294d086024fad
stuge <tuukkaveteli@gmail.com>
2021-07-11 11:31:17 +0300

    imports

--- part10/rate-repo/src/components/SingleRepo.jsx
+++ part10/rate-repo/src/components/SingleRepo.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import {View} from 'react-native';
 import {useParams} from 'react-router-native';
+import {useQuery} from '@apollo/client';
+
+import { GET_REPO_URL } from '../graphql/queries';
 
 const SingleRepo = () => {
   const {id} = useParams();
+  const [getRepoUrl, {data, loading, error}] = useQuery(GET_REPO_URL);
   console.log(`id`, id);
+
   return (
     <View>
             
