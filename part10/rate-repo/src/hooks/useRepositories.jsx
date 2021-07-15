7dd1f9914ff395d389791e7e79d201767a947e63
stuge <tuukkaveteli@gmail.com>
2021-07-15 10:57:25 +0300

    fetchRepositories hook now orders results

--- part10/rate-repo/src/hooks/useRepositories.jsx
+++ part10/rate-repo/src/hooks/useRepositories.jsx
@@ -2,15 +2,36 @@ import { useEffect } from 'react';
 import {useLazyQuery} from '@apollo/client';
 import {GET_REPOSITORIES} from '../graphql/queries';
 
-const useRepositories = () => {
+const SORTS = {
+  latest:{
+    orderBy: 'CREATED_AT',
+    orderDirection: 'DESC'
+  },
+  ratingHigh:{
+    orderBy: 'RATING_AVERAGE',
+    orderDirection: 'DESC'
+  },
+  ratingLow:{
+    orderBy: 'RATING_AVERAGE',
+    orderDirection: 'ASC'
+  },
+};
+
+const useRepositories = (selectedSort) => {
   const [fetchRepositories, { loading, data, refetch }] = useLazyQuery(
     GET_REPOSITORIES,
     {fetchPolicy: 'cache-and-network'}
   );
 
   useEffect(() => {
-    fetchRepositories();
-  }, []);
+    const sort = SORTS[selectedSort];
+    fetchRepositories({
+      variables: {
+        orderBy: sort.orderBy,
+        orderDirection: sort.orderDirection,
+      }
+    });
+  }, [selectedSort]);
 
   return { 
     repositories: data ? data.repositories : undefined,
